1d14a52a7dbf0adf5b141945cdad38cc0bb24e3f
thaimore <volodea.leyciu@gmail.com>
2023-12-07 19:01:24 +0200

    Memory leak fix

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 FROM eclipse-temurin:17-jdk-alpine
 VOLUME /tmp
 EXPOSE 8083
-ARG JAR_FILE=target/RestaurantService-0.0.1-SNAPSHOT.jar
-ADD ${JAR_FILE} RestaurantService-0.0.1-SNAPSHOT.jar
-ENTRYPOINT ["java","-jar","/RestaurantService-0.0.1-SNAPSHOT.jar"]
+ARG LOCATION
+ENV LOCATION ${LOCATION}
+ARG JAR_FILE
+ENV JAR_FILE ${JAR_FILE}
+ADD target/${JAR_FILE} ${JAR_FILE}
+CMD java -jar /${JAR_FILE} --spring.config.location=classpath:/${LOCATION}
--- src/main/java/com/org/rs/messaging/MessageRouting.java
+++ src/main/java/com/org/rs/messaging/MessageRouting.java
@@ -2,20 +2,25 @@ package com.org.rs.messaging;
 
 import com.org.rs.integration.MessageHandler;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import static com.org.ma.utils.Constants.RESTAURANT_CHANNEL;
 
 @Component
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class MessageRouting extends RouteBuilder {
+
+    @Value(value = "${spring.kafka.consumer.bootstrap-servers}")
+    private String brokerAddress;
 
     private MessageHandler handler;
 
     @Override
     public void configure() {
-        from("kafka:%s?brokers=172.18.0.1:9092".formatted(RESTAURANT_CHANNEL))
+        from("kafka:%s?brokers=%s".formatted(RESTAURANT_CHANNEL, brokerAddress))
                 .routeId("restaurant-channel")
                 .process(handler);
     }
